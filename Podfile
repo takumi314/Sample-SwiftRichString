bc1f9fd955988657bac8c736e1efec16f9190f05
takumi314 <isospin_symmetry@me.com>
2017-11-19 19:39:21 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Sample-SwiftRichString' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'SwiftRichString'
+
+  # Pods for Sample-SwiftRichString
+
+  target 'Sample-SwiftRichStringTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Sample-SwiftRichString/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample-SwiftRichString
+//
+//  Created by NishiokaKohei on 2017/11/19.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
