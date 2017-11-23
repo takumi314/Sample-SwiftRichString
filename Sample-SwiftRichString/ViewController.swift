b0a88dec6385008005b2840c6be076bd22fa4be6
takumi314 <isospin_symmetry@me.com>
2017-11-23 17:08:03 +0900

    update comments

--- Sample-SwiftRichString/ViewController.swift
+++ Sample-SwiftRichString/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
 
     private var label: UILabel!
 
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,7 +27,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - SwiftRichString configurations
