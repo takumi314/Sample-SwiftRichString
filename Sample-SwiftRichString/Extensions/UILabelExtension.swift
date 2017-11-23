b6efdba1017a74e93aa14c0bae3bceebb9a20e38
takumi314 <isospin_symmetry@me.com>
2017-11-23 17:33:40 +0900

    add extension file for UILabel

---
+++ Sample-SwiftRichString/Extensions/UILabelExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UILabelExtension.swift
+//  Sample-SwiftRichString
+//
+//  Created by NishiokaKohei on 2017/11/23.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+
+}
